79d1cc278bc0c41777bdcf83f10300df8a19a418
StarTux <ths.loy@gmail.com>
2024-04-02 03:10:57 +0200

    Rename

--- src/main/java/com/winthier/bans/sql/SQLSilentBan.java
+++ src/main/java/com/winthier/bans/sql/SQLSilentBan.java
@@ -1,5 +1,6 @@
 package com.winthier.bans.sql;
 
+import com.winthier.sql.SQLRow.Name;
 import com.winthier.sql.SQLRow.NotNull;
 import com.winthier.sql.SQLRow;
 import java.util.Date;
@@ -8,6 +9,7 @@ import lombok.Data;
 
 @Data
 @NotNull
+@Name("silent_bans")
 public final class SQLSilentBan implements SQLRow {
     @Id private Integer id;
     private UUID uuid;
